7a6fb374ac4bae01638525c3261ad5e83133a1ee
Elliot Ritchie <elliotritchie@hotmail.com>
2011-04-16 23:51:27 +0100

    added sample web project

---
+++ sample/NES.Sample.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace NES.Sample.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ sample/NES.Sample.Web/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NES.Sample.Web
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RegisterAllAreas();
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        private static void RegisterAllAreas()
+        {
+            AreaRegistration.RegisterAllAreas();
+        }
+
+        private static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        private static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
+            routes.IgnoreRoute("content/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
