64420971fd44a042224196f3b7fa0b539a033db0
Elliot Ritchie <elliotritchie@hotmail.com>
2011-05-14 14:41:25 +0100

    updated assembly info for use in nuspec

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
 
+[assembly: AssemblyCompany("Elliot Ritchie")]
+[assembly: AssemblyCopyright("Copyright © Elliot Ritchie 2011")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
--- src/NES/Properties/AssemblyInfo.cs
+++ src/NES/Properties/AssemblyInfo.cs
@@ -6,11 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NES")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET Event Sourcing")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NES")]
-[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
