09890b7335decb176f91c656b567c3d3724e4c73
Elliot Ritchie <elliotritchie@hotmail.com>
2011-09-10 11:19:36 +0100

    added CLSCompliant attribute

--- src/NES/Properties/AssemblyInfo.cs
+++ src/NES/Properties/AssemblyInfo.cs
@@ -1,33 +1,15 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("NES")]
 [assembly: AssemblyDescription(".NET Event Sourcing")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("NES")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2299cc33-4a12-4218-9259-2c03fa13fa05")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: InternalsVisibleTo("NES.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100217403244f058dae5517c49b7f0d2839263bd8062ace473821df37371d14f9b2772934ff8038a1489d9c90d504a2dfdaba56595317874754b6eeab539354c70e723013912506378e1b71b23f1d067c98521a9debc782b2b0bf81441a75dbf1fa2a41afea2a1fbc17f310d313e0e04bebc1f7da9e6b20263541e77efd78a643ce")]
