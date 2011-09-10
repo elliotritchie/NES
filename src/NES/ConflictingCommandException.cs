004b582fbffcea59357f0fe2b919badaa2369a32
Elliot Ritchie <elliotritchie@hotmail.com>
2011-09-10 11:49:28 +0100

    added Serializable attribute to ConflictingCommandException

--- src/NES/ConflictingCommandException.cs
+++ src/NES/ConflictingCommandException.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace NES
 {
+    [Serializable]
     public class ConflictingCommandException : Exception
     {
         public ConflictingCommandException(string message, Exception innerException)
