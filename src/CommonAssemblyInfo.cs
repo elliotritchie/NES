f3483989f81f2458bacaddf45611d32206813086
Elliot Ritchie <elliotritchie@hotmail.com>
2011-11-03 16:31:23 +0000

    updated version to 0.4

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCompany("Elliot Ritchie")]
 [assembly: AssemblyCopyright("Copyright © Elliot Ritchie 2011")]
-[assembly: AssemblyVersion("0.3.3.0")]
-[assembly: AssemblyFileVersion("0.3.3.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
