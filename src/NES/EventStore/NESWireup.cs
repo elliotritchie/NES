f69afb42a93310172af2e59abbfe62ad919be1ed
Elliot Ritchie <elliotritchie@hotmail.com>
2011-11-30 00:03:08 +0000

    changed commit dispatching to be synchronous

--- src/NES/EventStore/NESWireup.cs
+++ src/NES/EventStore/NESWireup.cs
@@ -14,7 +14,7 @@ namespace NES.EventStore
         {
             Container.Register<ISerialize>(new Serializer(Container.Resolve<ISerialize>(), () => DI.Current.Resolve<IEventSerializer>()));
             Container.Register<IPublishMessages>(new MessagePublisher(() => DI.Current.Resolve<IEventPublisher>()));
-            Container.Register<IDispatchCommits>(c => new AsynchronousDispatcher(c.Resolve<IPublishMessages>(), c.Resolve<IPersistStreams>()));
+            Container.Register<IDispatchCommits>(c => new SynchronousDispatcher(c.Resolve<IPublishMessages>(), c.Resolve<IPersistStreams>()));
 
             DI.Current.Register<IEventStore, IStoreEvents>(eventStore => new EventStoreAdapter(eventStore));
             DI.Current.Register(() => Container.Resolve<IStoreEvents>());
