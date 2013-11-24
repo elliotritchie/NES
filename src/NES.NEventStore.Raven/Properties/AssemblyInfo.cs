775cac1b7b1978b8b2d73a6e02268099862631cd
Elliot Ritchie <elliotritchie@hotmail.com>
2013-11-24 12:30:28 +0000

    renamed NES.EventStore projects to NES.NEventStore and removed strong naming

--- src/NES.NEventStore.Raven/Properties/AssemblyInfo.cs
+++ src/NES.NEventStore.Raven/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NES.EventStore.Raven")]
+[assembly: AssemblyTitle("NES.NEventStore.Raven")]
 [assembly: AssemblyDescription(".NET Event Sourcing")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("NES")]
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 [assembly: Guid("67fcc0f9-4dc5-4ddb-843b-bfd865fe025f")]
-[assembly: InternalsVisibleTo("NES.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100217403244f058dae5517c49b7f0d2839263bd8062ace473821df37371d14f9b2772934ff8038a1489d9c90d504a2dfdaba56595317874754b6eeab539354c70e723013912506378e1b71b23f1d067c98521a9debc782b2b0bf81441a75dbf1fa2a41afea2a1fbc17f310d313e0e04bebc1f7da9e6b20263541e77efd78a643ce")]
+[assembly: InternalsVisibleTo("NES.Tests")]
